0f7d074f75f0d8b914e1d52c5a84206cd3305bb5
(Lab 171)Liang Bo Wang <ccwang002@gmail.com>
2014-01-01 04:56:36 +0800

    update grid search, combine libSVM and libLINEAR
    also use 60x60 as input.
    
    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
    WWWW   WWW   WWWWWW:    :WWWWWW       'WWWW       'WWW. 'WWW' .WWWW
    WWWW   WWW   WWWWW:  ..  :WWWWW   WW.  'WWW   WW.  'WWW. 'W' .WWWWW
    WWWW         WWWW:   WW   :WWWW   WW:   WWW   WW:   WWWW. ' .WWWWWW
    WWWW   WWW   WWWW'        'WWWW       .:WWW       .:WWWWW   WWWWWWW
    WWWW   WWW   WWW:   WWWW   :WWW   WWWWWWWWW   WWWWWWWWWWW   WWWWWWW
    WWWW   WWW   WWW'  .WWWW.  'WWW   WWWWWWWWW   WWWWWWWWWWW   WWWWWWW
    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
      :W   W: :WWWWW: WW.     .WW  WW   WW :WWWWW:    .W.    :WWWW:.
      :W:  W: :W: WW: :W:     :W:  'W: :W' :W: WW:   .:W:.   :W: 'W:
      :WW: W: :W:      W:.   .:W    'W:W'  :W:       :W W:   :W: .W:
      :WWW:W: :WWWW:   :W:.W.:W:     :W:   :WWWW:   :WWWWW:  :WWWW:'
      :W :WW: :W:       WWWWWWW      :W:   :W:     .WW' 'WW. :W':W.
      :W  :W: :W: WW:   :W:':W:      :W:   :W: WW: :W:   :W: :W ':W.
      :W   W: :WWWWW:    W: :W       :W:   :WWWWW: WW'   'WW :W  ':W.
    
            __
          .'`  `'.
         /        \ _
        ;      __.'` `'.
        |   .'`  `'.    \
        ;  / HAPPY  \    ;
         \;   NEW    ;   |
          | Y E A R  |   ;   _
          ; 2 0 1 4  ;-./-_-` '-.
          /\        /_(;'/ `\()  '.
         ;  '.__  .'\|| '    |     '.
         |      ),\| \\      \()    (\
         ;        \ \|/   __/    ()   \  __
          \        \||\.~'_ `'.;-.___.~'` _'~.
           '.__  _/|/|/{ (_`.'         '.`_) }
               `)/`\\\\ \ .'  _ 0_._0 _  '. /     .,_
                     \|| } -.'   (_)   '.- {    _{   `\
                      \|{_ / '.___|___.' \  }  //`._   |
                    /`    \     |   |     }  }:'-. ()``'"--..==,
                   {      ,}    \-"-/   .'  } {,`-'.      (//>`\>
                  {`   _./|\._.  '-'  ._ .~` /`    ;'.()  //>  |>
                  {     {///(  `-.-.-`  ) _.'     /   '. ||>   />
                   \     \|\);--`( )`--`(`       }      `\\>_.'>
                    ;  _/`/(__.'/`-'.,__/`,    .`         `"""`
                   .-'`     ;-.(     \_(;  \ .'     .--,
                  (`-._   ./   `       '.   `-._..~` /o\\
               jgs `'-;/``.              `;-"`:     |oo||
              .--._ _.' .  \      o       ;  .      |  /|
             /.-.  `     .  '._        _.'  '       \_//
             ||oo\        `.   `'-----`  _.~`--..__,..'
             |\o  |       .~`'--......--'
              \'._/   _.~`
               `.__.-'

--- SVM_gridsearch.py
+++ SVM_gridsearch.py
@@ -5,52 +5,57 @@ from sklearn import svm, grid_search, metrics
 from sklearn.cross_validation import train_test_split
 
 
-CSV_TRAIN = "dataset/train_na2zero.csv"
-CSV_TEST = "dataset/test_na2zero.csv"
+CSV_TRAIN = "dataset/train_zero_60x60.csv"
+CSV_TEST = "dataset/test_zero_60x60.csv"
 
+GRID_RESULT = "results/grid_CV/combined_60x60_0101_1.txt"
 
 df_train = pd.read_csv(CSV_TRAIN)  #, nrows=1000)
 Y = df_train.y
 X = df_train.iloc[:, 1:].values
 
 # set parameter for grid search
-parameters = {
-    'kernel': ['poly'],
-    'C': [10 ** i for i in range(-7, 5)],
-    # 'gamma': [10 ** i for i in range(-5, -2)]
-    'degree': [i for i in range(2, 6)]
-}
-rbf_clf = svm.SVC()
+param_list  = [
+    {'kernel': ['rbf', 'poly', 'linear'],
+     'C': [10 ** i for i in range(-2, 6)],
+     'gamma': [10 ** i for i in range(-6, -2)],
+     'degree': [i for i in range(2, 5)]},
+    {'C': [10 ** i for i in range(-4, 7)]}
+]
+
+svm_clf = svm.SVC()
 lin_clf = svm.LinearSVC()
-poly_clf = svm.SVC()
+# poly_clf = svm.SVC()
 
 train_X, test_X, train_Y, test_Y = train_test_split(
     X, Y, test_size=0.25, random_state=5
 )
 
-grid_cv = grid_search.GridSearchCV(poly_clf, parameters, n_jobs=14, verbose=2)
+for clf, param in zip((svm_clf, lin_clf), param_list):
+    grid_cv = grid_search.GridSearchCV(clf, param, n_jobs=14, verbose=1)
 
-# training
-grid_cv.fit(train_X, train_Y)
-# print best estimator
+    # training
+    grid_cv.fit(train_X, train_Y)
 
-print(
-    "\n\n\n>>>> [best parameter from grid searching] <<<<\n",
-    grid_cv.best_estimator_,
-    "\n\n\n"
-)
-with open("poly_SVM_grid_result.txt", "w") as f:
-    for params, mean_score, scores in grid_cv.grid_scores_:
-        row_msg = (
-            "%0.3f (+/-%0.03f) for %r"
-            % (mean_score, scores.std() / 2, params)
+    # print best estimator
+    with open(GRID_RESULT, "a") as f:
+        msg = (
+            "\n\n\n>>>> [best parameter from grid searching] <<<<\n",
+            grid_cv.best_estimator_,
+            "\n\n\n"
         )
-        print(row_msg)
-        print(row_msg, file=f)
+        print(*msg)
+        print(*msg, file=f)
+        for params, mean_score, scores in grid_cv.grid_scores_:
+            row_msg = (
+                "%0.3f (+/-%0.03f) for %r"
+                % (mean_score, scores.std() / 2, params)
+            )
+            print(row_msg)
+            print(row_msg, file=f)
 
-# testing
-pred_Y = grid_cv.predict(test_X)
-
-# report
-print(metrics.classification_report(test_Y, pred_Y))
-print(metrics.confusion_matrix(test_Y, pred_Y))
+    # testing
+    pred_Y = grid_cv.predict(test_X)
+    # report
+    print(metrics.classification_report(test_Y, pred_Y))
+    print(metrics.confusion_matrix(test_Y, pred_Y))
