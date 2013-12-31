098e92a6681ee43f6dbd2634a46929c418e7fcf5
(Lab 171)Liang Bo Wang <ccwang002@gmail.com>
2013-12-31 13:31:14 +0800

    fix gridsearch error T___T
    Also, test in the end is added back

--- SVM_gridsearch.py
+++ SVM_gridsearch.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
 # import pylab as pl
 from sklearn import svm, grid_search, metrics
@@ -9,42 +9,45 @@ CSV_TRAIN = "dataset/train_na2zero.csv"
 CSV_TEST = "dataset/test_na2zero.csv"
 
 
-df_train = pd.read_csv(CSV_TRAIN)   # nrows=3000)
+df_train = pd.read_csv(CSV_TRAIN)  #, nrows=1000)
 Y = df_train.y
 X = df_train.iloc[:, 1:].values
 
 # set parameter for grid search
 parameters = {
-    'kernel': 'rbf',
+    'kernel': ['rbf'],
     'C': [10 ** i for i in range(-3, 3)],
-    'gamma': [10 ** i for i in range(-6, 2)]
+    'gamma': [10 ** i for i in range(-8, 2)]
 }
 rbf_clf = svm.SVC()
-lin_clf = svm.LinearSVC()
 
 train_X, test_X, train_Y, test_Y = train_test_split(
     X, Y, test_size=0.25, random_state=5
 )
 
-for clf in (rbf_clf, lin_clf):
-    grid_cv = grid_search.GridSearchCV(clf, parameters, n_jobs=12, verbose=2)
-    # training
-    grid_cv.fit(train_X, train_Y)
-    # print best estimator
-    print(
-        "[best parameter from grid searching]\n",
-        grid_cv.best_estimator_
-    )
-    for params, mean_score, scores in clf.grid_scores_:
-        print(
+grid_cv = grid_search.GridSearchCV(rbf_clf, parameters, n_jobs=12, verbose=2)
+
+# training
+grid_cv.fit(train_X, train_Y)
+# print best estimator
+
+print(
+    "\n\n\n>>>> [best parameter from grid searching] <<<<",
+    grid_cv.best_estimator_,
+    "\n\n\n"
+)
+with open("rbf_SVM_grid_result.txt", "w") as f:
+    for params, mean_score, scores in grid_cv.grid_scores_:
+        row_msg = (
             "%0.3f (+/-%0.03f) for %r"
-            % (mean_score, scores.std() // 2, params)
+            % (mean_score, scores.std() / 2, params)
         )
+        print(row_msg)
+        print(row_msg, file=f)
 
 # testing
-# expected = np.array(Y[n_samples / 2:])
-# predicted = rbf_svc.predict(X[n_samples / 2:])
+pred_Y = grid_cv.predict(test_X)
 
 # report
-# print(metrics.classification_report(expected, predicted))
-# print( metrics.confusion_matrix(expected, predicted))
+print(metrics.classification_report(test_Y, pred_Y))
+print(metrics.confusion_matrix(test_Y, pred_Y))
