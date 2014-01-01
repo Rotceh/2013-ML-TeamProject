93a374cfaa49fa5bcedaf0cb7c43e358d0c3b9b0
Liang Bo Wang <ccwang002@gmail.com>
2014-01-01 17:36:25 +0800

    add mean modified dataset, with script
    previous generated xz file will be removed

--- util/filling_missing_data.py
+++ util/filling_missing_data.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 def add_missing_data_with_group_mean(one_group_label, one_group_df):
-    one_group_data_array = one_group.values[:,1:]
+    one_group_data_array = one_group_df.values[:,1:]
     one_group_mean = np.mean(one_group_data_array,axis=0)
     one_group_new_data_array = np.apply_along_axis(lambda one_row: one_row + one_group_mean, 1, one_group_data_array)
     N_ROW = one_group_new_data_array.shape[0]
@@ -19,10 +19,11 @@ def add_missing_data_with_group_mean(one_group_label, one_group_df):
 
 if __name__ == '__main__':
     pass
-    
+
     import pandas as pd
-    
+
     CSV_TRAIN = "../dataset/train_zero_60x60.csv"
     df_part = pd.read_csv(CSV_TRAIN, nrows=1200).fillna(0)
     grouped = df_part.groupby("y")
-    print [add_missing_data_with_group_mean(one_group_label,one_group) for one_group_label,one_group in grouped ]
+    print([add_missing_data_with_group_mean(one_group_label,one_group) for
+           one_group_label,one_group in grouped ])
