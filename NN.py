cfe445bd4cb42ad25e2238dd7e27da2264584029
C.Y. Yen <yen.chaiyi@gmail.com>
2014-01-02 12:02:28 +0800

    remove pdb XD

--- NN.py
+++ NN.py
@@ -20,13 +20,14 @@ Y = df_train.y
 Y = Y -1 # in order to make target in the range of [0, 1, 2, 3, ...., 11]
 X = df_train.iloc[:, 1:].values
 
-df_test = pd.read_csv(CSV_TEST, nrows=1200)
-test_X = df_test.iloc[:, 1:].values
-
 alldata = ClassificationDataSet(inp=X.shape[1], target=1, nb_classes=12)
-for i in range(len(Y)):
+for i in range(X.shape[0]):
     alldata.addSample(X[i, :], [Y[i]])
 alldata._convertToOneOfMany()
+
+df_test = pd.read_csv(CSV_TEST)
+test_X = df_test.iloc[:, 1:].values
+
 
 
 print "Number of training patterns: ", len(alldata)
@@ -36,20 +37,30 @@ print alldata['input'][0], alldata['target'][0], alldata['class'][0]
 
 #############################################################################
 # fnn
-n = buildNetwork(alldata.indim, 20,20, alldata.outdim, outclass=SoftmaxLayer, bias=True)
+n = buildNetwork(alldata.indim, 1000, 100, 10,  alldata.outdim, outclass=SoftmaxLayer, bias=True)
 print(n)
 
 #############################################################################
 
 trainer = BackpropTrainer(n, dataset=alldata, learningrate=0.01, momentum=0.1, verbose=True, weightdecay=0.01)
-model = trainer.trainUntilConvergence(maxEpochs=5, validationProportion=0.25)
+model = trainer.trainUntilConvergence(maxEpochs=200, validationProportion=0.25)
 print("[ the best parameter for minimal validation error]", n.params)
-print(trainer.testOnClassData(alldata))
+print( trainer.testOnClassData(alldata) )
 allresult = percentError(trainer.testOnClassData(dataset = alldata) ,alldata['class'])
 print("  train error: %5.2f%%" % allresult)
 
-import pdb
-pdb.set_trace()
+##
+import numpy
+print "Predicting with the neural network"
+answerlist = []
+
+for row in range(test_X.shape[0]):
+    answer = numpy.argmax(n.activate(test_X[row, :]))
+    answerlist.append(answer)
+
+print(answerlist)
+
+
 
 
 ####################################################
