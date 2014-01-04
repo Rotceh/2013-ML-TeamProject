993deef1e30720e8d1d71918f8e385db28472df0
ChiaChi <c3h3.tw@gmail.com>
2014-01-04 15:02:43 +0800

    add randomforest model with rstudio

--- r/rf.R
+++ r/rf.R
@@ -4,9 +4,12 @@ load("~/c3h3works/MLDM_Projects/2013-ML-TeamProject/dataset/testing_data_zeros_6
 library(fpc)
 
 View(training_df[,-1])
-rf_model <- randomForest(y ~ ., data=training_df,ntree=2000, mtry = 30, importance=TRUE,proximity=TRUE)
+rf_model <- randomForest(y ~ ., data=training_df,ntree=2000, mtry = 120, importance=TRUE,proximity=TRUE)
 
 testing_df_pred = predict(rf_model,testing_df)
-write.csv(testing_df_pred,file="output_try_rf.csv", quote = FALSE,row.names = FALSE)
+testing_df_pred2 = predict(rf_model,testing_df)
+testing_df_pred3 = predict(rf_model,testing_df)
+write.csv(testing_df_pred3,file="output_try_rf_nt2000_mt120.csv", quote = FALSE,row.names = FALSE)
 
-save(rf_model, file="rf_model_default.RData")
+save(rf_model, file="rf_model_nt2000_mt120.RData")
+
