2849cb3eaad76dafc6b8610510811b5ccff19fa4
c3h3 <c3h3.tw@gmail.com>
2014-01-05 11:05:08 +0800

    checkout duplicate jobs

--- bows_celery/tasks.py
+++ bows_celery/tasks.py
@@ -148,38 +148,47 @@ def get_sift_descs(im_data):
 
 @celery.task(ignore_result = True)
 def compute_two_image_matching_scores(img1_data, img2_data):
+    
     xx = img2_data
     one_ix_data = img1_data
-    
-    norm = cv2.NORM_L2
-    bfmatcher = cv2.BFMatcher(norm)
-    
-    MATCH_DIST_UPPER_BOUND = 300    
-    
-    
-    # Match descriptors.
-    matches = bfmatcher.match(np.array(get_sift_descs(xx).info),
-                              np.array(get_sift_descs(one_ix_data).info))
-    
-    print matches
-    print "len(matches) = ",len(matches)
-    filtered_matches = [x for x in matches if x.distance <= MATCH_DIST_UPPER_BOUND]
-    print "len(filtered_matches) = ",len(filtered_matches)
-    
-    
-    matching_score = 0 if len(matches) == 0 else float(len(filtered_matches)) / float(len(matches))
-    
-    print "matching_score = ",matching_score
     
     
     score_data, created = NewMatchingScores.objects.get_or_create(ix1 = one_ix_data.ix, 
                                                                   c1 = one_ix_data.class_id,
                                                                   ix2 = xx.ix, 
                                                                   c2 = xx.class_id)
-    score_data.score = matching_score
-    score_data.save()
-
-    print "score_data = ",score_data
+    
+    if created:
+        
+        norm = cv2.NORM_L2
+        bfmatcher = cv2.BFMatcher(norm)
+        
+        MATCH_DIST_UPPER_BOUND = 300    
+        
+        
+        # Match descriptors.
+        matches = bfmatcher.match(np.array(get_sift_descs(xx).info),
+                                  np.array(get_sift_descs(one_ix_data).info))
+        
+        print matches
+        print "len(matches) = ",len(matches)
+        filtered_matches = [x for x in matches if x.distance <= MATCH_DIST_UPPER_BOUND]
+        print "len(filtered_matches) = ",len(filtered_matches)
+        
+        
+        matching_score = 0 if len(matches) == 0 else float(len(filtered_matches)) / float(len(matches))
+        
+        print "matching_score = ",matching_score
+        
+        
+        score_data, created = NewMatchingScores.objects.get_or_create(ix1 = one_ix_data.ix, 
+                                                                      c1 = one_ix_data.class_id,
+                                                                      ix2 = xx.ix, 
+                                                                      c2 = xx.class_id)
+        score_data.score = matching_score
+        score_data.save()
+    
+        print "score_data = ",score_data
 
 
 
